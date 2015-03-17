6c36b5b068b674e5a2f111e03964ff96ee125ea2
gauthiier <d@gauthiier.info>
2015-03-17 08:52:29 +0100

    Initial Commit

---
+++ plfr/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  plfr
+//
+//  Created by David Gauthier on 17/03/15.
+//  Copyright (c) 2015 David Gauthier. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
